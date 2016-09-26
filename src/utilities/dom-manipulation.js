deb03d494d47dfb842da4ecd03159bc0ddd3ab3d
Pietro Peluso <pietropeluso@Pietros-MacBook-Pro.local>
2016-09-26 14:40:38 +0100

    - changing the structure of each result item to display as clickable tiles - removing the header from the index.html for a cleaner look - styles added

--- src/utilities/dom-manipulation.js
+++ src/utilities/dom-manipulation.js
@@ -6,20 +6,35 @@ const populateResult = (container, data) => {
 };
 
 const createSingleTile = (data) => {
-  const el = document.createElement('div');
-  el.className = 'movie-tile';
+
+  const tile = document.createElement('a');
+  // tile.className = 'movie-tile';
+  tile.setAttribute('href', `https://www.themoviedb.org/movie/${data.id}`);
+  tile.setAttribute('target', '_blank');
+
+
+  // creating container
+  const div = document.createElement('div');
+  div.className = 'movie-tile';
 
   // create a poster image inside the tile
   const img = document.createElement('img');
   img.src = `https://image.tmdb.org/t/p/w300_and_h450_bestv2/${data.poster_path}`;
-  el.appendChild(img);
+  div.appendChild(img);
 
   // adding a label to display the movie title
   const label = document.createElement('p');
+  label.className = 'movie-title';
   label.innerHTML = data.title;
-  el.appendChild(label)
+  div.appendChild(label)
 
-  return el;
+  const releaseDate = document.createElement('p');
+  releaseDate.className = 'movie-release-date';
+  releaseDate.innerHTML = data.release_date;
+  div.appendChild(releaseDate);
+
+  tile.appendChild(div);
+  return tile;
 }
 
 const clearResult = (container) => {
